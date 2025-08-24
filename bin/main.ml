f25fe1361e9af4f56876148ec5543295762caf7f
nqr2 <naqr2@proton.me>
2025-08-23 23:23:48 -0300

    Add command-line arguments.

--- bin/main.ml
+++ bin/main.ml
@@ -1,5 +1,20 @@
-let () =
-  let path = "test.tw" in
+let subcommandp = ref true
+let subcommands = [ ("check", []) ] |> List.to_seq |> Hashtbl.of_seq
+let specs = ref []
+let files = ref []
+
+let anon s =
+  if !subcommandp then (
+    subcommandp := false;
+    try specs := Hashtbl.find subcommands s
+    with Not_found -> failwith ("unknown command: " ^ s))
+  else files := s :: !files
+
+let process_file path =
   let input = In_channel.open_text path in
   let _ast = Option.get @@ Parse.parse_chan ~fname:path input in
   ()
+
+let () =
+  Arg.parse_dynamic specs anon "";
+  List.iter process_file !files
