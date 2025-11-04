571adc2d939f65c134d1b7d1e15c1c1dcd94fdba
nqr2 <naqr2@proton.me>
2025-11-04 15:00:07 -0300

    TODO: Emit "reports" and handle them somewhere else.

---
+++ frontend/report.ml
@@ -0,0 +1,25 @@
+type kind = Note | Warning | Error
+
+let pp_kind f =
+  let open Util.Combinator in
+  Format.pp_print_string f <@@> function
+  | Note -> "NOTE"
+  | Warning -> "WARNING"
+  | Error -> "ERROR"
+
+type mark = Mark of { span : Loc.span; text : string }
+
+type t =
+  | Report of { path : string; kind : kind; text : string; marks : mark list }
+
+let present (Report r) =
+  let errored = ref false in
+  Format.printf "[%s ?:?] %a: %s" r.path pp_kind r.kind r.text;
+
+  (* TODO: display r.marks *)
+  if r.kind == Error then errored := true;
+
+  !errored
+
+let reports = ref []
+let add (r : t) = reports := r :: !reports
